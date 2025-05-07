6a96d6e391f8c229b274467f240e7e321043c3f8
kenan zengin <kenan.zengin@cropy.ai>
2025-05-07 18:43:50 +0300

    env update

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiKJSr8BSm3i_KPAVhAOjOADWMhjW229g",
-  authDomain: "anitopla.firebaseapp.com",
-  projectId: "anitopla",
-  storageBucket: "anitopla.firebasestorage.app",
-  messagingSenderId: "803969443911",
-  appId: "1:803969443911:web:672feb1959a989cae1e962",
-  measurementId: "G-RK9F6P00T1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig);
